3bc4584a0dc6dced32f301d2e569e8478353a190
Ahn Honghyun <anh2073@naver.com>
2026-03-13 17:13:49 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,19 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars:Variants = {
-  start: {scale: 0},
-  end: {scale: 1, rotateZ:360, transition:{type: "spring", delay: 0.5}}
-};
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box initial="start" animate="end">
+      </Box>
     </Wrapper>
   );
 }
