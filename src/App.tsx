2e8952e20fe3f772fddf3b1a8143763480518a25
Ahn Honghyun <anh2073@naver.com>
2026-03-16 10:24:09 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,15 @@ const boxVariants:Variants = {
     scale: 0.5,
   },
   end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
   },
 };
 
